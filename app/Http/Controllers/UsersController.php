f57da279ea94f9d84b478e0389f185cc163ee16f
Jude <j_norvor@hotmail.com>
2022-03-01 18:53:53 +0000

    Uniform response format

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,74 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\ApiResponseService;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 
 class UsersController extends Controller
 {
     /**
+     * @param ApiResponseService $apiResponseService
+     */
+    public function __construct(ApiResponseService $apiResponseService)
+    {
+        $this->apiResponseService = $apiResponseService;
+    }
+    /**
      * Display a listing of the resource.
      *
-     * @return AnonymousResourceCollection
+     * @return JsonResponse
      */
     public function index()
     {
-        return UserResource::collection(User::all());
+        return $this->apiResponseService->respondWithResourceCollection(UserResource::collection(User::all()));
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param Request $request
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function store(StoreUserRequest $request)
     {
         $userData = $request->getSanitized();
         $storedUser = User::create($userData);
         if($storedUser)
-            return $storedUser;
-//        Return error
+            $this->apiResponseService->respondCreated($storedUser);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\User  $user
-     * @return UserResource
+     * @param User $user
+     * @return JsonResponse
      */
     public function show(User $user)
     {
-        return new UserResource($user);
+        return $this->apiResponseService->respondWithResource(new UserResource($user),'User found');
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param Request $request
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
+     * @param User $user
+     * @return JsonResponse
      */
     public function update(UpdateUserRequest $request, User $user)
     {
         $userDataToUpdate = $request->getSanitized();
-        $user->update($userDataToUpdate);
-        return 'user updated';
+        $userUpdated = $user->update($userDataToUpdate);
+        if($userUpdated)
+            return $this->apiResponseService->respondSuccess('User updated');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
+     * @param User $user
+     * @return JsonResponse
      */
     public function destroy(User $user)
     {
-        $user->delete();
-        return 'user deleted';
+        $userDeleted = $user->delete();
+        if($userDeleted)
+            return $this->apiResponseService->respondSuccess('User deleted');
     }
 }
---
+++ app/Http/Services/ApiResponseService.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http;
+
+use App\Http\Resources\Empty\EmptyResource;
+use App\Http\Resources\Empty\EmptyResourceCollection;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ApiResponseService
+{
+    /**
+     * Respond with success.
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    public function respondSuccess($message = '')
+    {
+        return $this->apiResponse(['success' => true, 'message' => $message]);
+    }
+
+    /**
+     * Respond with created.
+     *
+     * @param $data
+     *
+     * @return JsonResponse
+     */
+    public function respondCreated($data)
+    {
+        return $this->apiResponse($data, 201);
+    }
+
+    /**
+     * Respond with no content.
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    public function respondNoContent($message = 'No Content Found')
+    {
+        return $this->apiResponse(['success' => false, 'message' => $message], 200);
+    }
+
+    /**
+     * Respond with no content.
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    protected function respondNoContentResource($message = 'No Content Found')
+    {
+        return $this->respondWithResource(new EmptyResource([]), $message);
+    }
+    /**
+     * Respond with no content.
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    public function respondNoContentResourceCollection($message = 'No Content Found')
+    {
+        return $this->respondWithResourceCollection(new EmptyResourceCollection([]), $message);
+    }
+
+    public function respondWithResource(JsonResource $resource, $message = null, $statusCode = 200, $headers = [])
+    {
+        return $this->apiResponse(
+            [
+                'success' => true,
+                'result' => $resource,
+                'message' => $message
+            ], $statusCode, $headers
+        );
+    }
+
+    /**
+     * @param ResourceCollection $resourceCollection
+     * @param null $message
+     * @param int $statusCode
+     * @param array $headers
+     * @return JsonResponse
+     */
+    public function respondWithResourceCollection(ResourceCollection $resourceCollection, $message = null, $statusCode = 200, $headers = [])
+    {
+        return $this->apiResponse(
+            [
+                'success' => true,
+                'result' => $resourceCollection->response()->getData()
+            ], $statusCode, $headers
+        );
+    }
+
+    /**
+     * Return generic json response with the given data.
+     *
+     * @param       $data
+     * @param int $statusCode
+     * @param array $headers
+     *
+     * @return JsonResponse
+     */
+    protected function apiResponse($data = [], $statusCode = 200, $headers = [])
+    {
+        $responseStructure = [
+            'success' => $data['success'],
+            'message' => $data['message'] ?? null,
+            'result' => $data['result'] ?? null,
+        ];
+        if (isset($data['errors'])) {
+            $responseStructure['errors'] = $data['errors'];
+        }
+        if (isset($data['status'])) {
+            $statusCode = $data['status'];
+        }
+
+        return response()->json(
+            $responseStructure, $statusCode, $headers
+        );
+    }
+}
